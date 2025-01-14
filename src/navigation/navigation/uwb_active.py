9101a040dd3fceba26a78c0cc80622fefec296af
Wouter Klaassen <wr.klaassen@student.avans.nl>
2025-01-14 11:46:00 +0100

    harvest action en navigation tussen commit

---
+++ src/navigation/navigation/uwb_active.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import node
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float64
+import serial
+import time
+import random
+from dwm1001_apiCommands import DWM1001_API_COMMANDS
+
+
+def main():
+    print('Hi from navigation.')
+
+
+if __name__ == '__main__':
+    main()
